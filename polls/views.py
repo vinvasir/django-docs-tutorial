d681ece429ec8c5d2cacfa46bdfa320ad541a0b7
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-09-28 13:31:38 -0400

    added 404 exception and template to the detail view

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
+from django.http import Http404
 from django.http import HttpResponse
-from django.shortcuts import render
 
 from .models import Question
 
@@ -13,7 +13,11 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")  
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     return HttpResponse("You're looking at the results of question %s." % question_id)
